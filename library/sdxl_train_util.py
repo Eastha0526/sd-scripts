c716229cee5aa322abfb52d631b9fca77d1849b0
aria1th <35677394+aria1th@users.noreply.github.com>
2024-03-30 06:27:38 +0900

    fix

--- library/sdxl_train_util.py
+++ library/sdxl_train_util.py
@@ -26,7 +26,6 @@ TOKENIZER2_PATH = "laion/CLIP-ViT-bigG-14-laion2B-39B-b160k"
 
 
 def load_target_model(args, accelerator:Accelerator, model_version: str, weight_dtype):
-    logger.info(f"loading model for process {pid}/{accelerator.state.num_processes}")
     model_dtype = match_mixed_precision(args, weight_dtype)  # prepare fp16/bf16
     logger.info(f"loading model for process {accelerator.state.process_index}/{accelerator.state.num_processes}")
     start_time = time.time()
